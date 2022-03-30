a3cc65931c7890aa7a7799aa4b99640c0d89f61a
Yannic Beyer <y.beyer@tu-bs.de>
2022-03-30 18:17:17 +0200

    Copter INDI: adjust loiter flight mode parameters to LADAC update

--- LADAC_params/Bebop2/control/fm_copter_loiter_indi_params_bebop2.m
+++ LADAC_params/Bebop2/control/fm_copter_loiter_indi_params_bebop2.m
@@ -7,17 +7,32 @@
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
-% altitude hold flight mode
- fm_loiter.fm_alt = fmCopterAltHoldIndiLoadParams( ...
-  'fm_copter_alt_hold_indi_params_bebop2' );
+% attitude controller
+fm_loiter.atti_cntrl = cntrlAttiRedIndiLoadParams( ...
+    'cntrl_atti_red_indi_params_bebop2');
+
+% attitude controller
+fm_loiter.alt_cntrl = cntrlAltIndiLoadParams( ...
+    'cntrl_alt_indi_params_bebop2' );
 
 % horizontal position controller
 fm_loiter.horiz_pos_cntrl = cntrlHorizPosIndiLoadParams( ...
     'cntrl_horiz_pos_indi_params_bebop2' );
 
+% control effectiveness
+fm_loiter.cntrl_effect = controlEffectivenessLoadParams( ...
+    'control_effectiveness_params_bebop2' );
+
+% control allocation
+fm_loiter.ca = controlAllocationWlsLoadParams( ...
+    'control_allocation_wls_params_quadcopter' );
+
 % sensor filter
 fm_loiter.sens_filt = indiSensFiltLoadParams( ...
     'indi_sens_filt_params_default' );
+
+% motor time constant, in s
+fm_loiter.motor_time_constant = 0.0138;
 
 % flight mode sample time, in s
 fm_loiter.sample_time = 0.0025;
