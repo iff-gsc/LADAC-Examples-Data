070b6adc5df1096bd9db1b41a53f6467d7a0c7ba
Yannic Beyer <y.beyer@tu-bs.de>
2022-08-04 11:50:39 +0200

    Adjust initial FlightGear position to LADAC
    The initial FlightGear position is now the same as
    in reference_position_params_default.m in LADAC.
    Only a higher altitude is used.

--- FlightGear/runfg_IRIS.sh
+++ FlightGear/runfg_IRIS.sh
@@ -18,6 +18,6 @@ fgfs --fg-aircraft=FlightGear/IRIS --aircraft=IRIS --fdm=null \
 	--native-fdm=socket,in,30,localhost,5502,udp \
 	--native-ctrls=socket,out,30,127.0.0.1,5503,udp --fog-fastest \
 	--disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound \
-	--in-air --enable-freeze --airport=KSFO --runway=10L --altitude=7224 \
+	--in-air --enable-freeze --lat=37.6117 --lon=-122.3782 --altitude=7224 \
 	--heading=113 --offset-distance=4.72 --offset-azimuth=0 \
 	--geometry=640x480 --max-fps=30 --enable-terrasync
