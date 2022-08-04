629fb704bb468483e23e12683ec524922b798960
Yannic Beyer <y.beyer@tu-bs.de>
2022-08-04 11:24:39 +0200

    Fix IRIS start script and initial view

--- FlightGear/runfg_IRIS.sh
+++ FlightGear/runfg_IRIS.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+# Get directory of the script
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+AIRCRAFTPATH=$SCRIPTPATH/../FlightGear
+
+# Set FG_AIRCRAFT environment variable
+export FG_AIRCRAFT=$AIRCRAFTPATH:${FG_AIRCRAFT}
+
+#cd /usr/share/games/flightgear
+
 #setenv LD_LIBRARY_PATH /usr/share/games/flightgear/lib:$LD_LIBRARY_PATH
 #setenv FG_ROOT /usr/share/games/flightgear/data
 #setenv FG_SCENERY /usr/share/games/flightgear/Scenery:$FG_ROOT/Scenery:$FG_ROOT/WorldScenery
@@ -10,5 +20,4 @@ fgfs --fg-aircraft=FlightGear/IRIS --aircraft=IRIS --fdm=null \
 	--disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound \
 	--in-air --enable-freeze --airport=KSFO --runway=10L --altitude=7224 \
 	--heading=113 --offset-distance=4.72 --offset-azimuth=0 \
-	--geometry=640x480 --max-fps=30 --prop:/sim/view=2
-#--prop:/sim/current-view/view-number=2
+	--geometry=640x480 --max-fps=30 --enable-terrasync
