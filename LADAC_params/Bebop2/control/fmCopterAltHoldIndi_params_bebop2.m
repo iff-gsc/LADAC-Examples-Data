73f3aa0e8725a9a6562f2b5d6a5ead1149d10b68
Yannic Beyer <y.beyer@tu-bs.de>
2022-12-11 20:46:02 +0100

    Adjust Bebop2 altitude hold flight mode parameters to latest LADAC

--- LADAC_params/Bebop2/control/fmCopterAltHoldIndi_params_bebop2.m
+++ LADAC_params/Bebop2/control/fmCopterAltHoldIndi_params_bebop2.m
@@ -8,27 +8,23 @@
 % *************************************************************************
 
 % attitude controller
-fm_alt_hold.atc = cntrlAttiRedIndiLoadParams( ...
-    'cntrlAttiRedIndi_params_bebop2' );
+param.atc = loadParams( 'cntrlAttiRedIndi_params_bebop2' );
 
 % attitude controller
-fm_alt_hold.psc = cntrlPosNdiLoadParams( ...
-    'cntrlPosNdi_params_bebop2' );
+param.psc = loadParams( 'cntrlPosNdi_params_bebop2' );
 
 % control effectiveness
-fm_alt_hold.cntrl_effect = controlEffectivenessLoadParams( ...
-    'control_effectiveness_params_bebop2' );
+param.cep = loadParams( 'indiCeProp_params_bebop2' );
+param.ceb = loadParams( 'indiCeBody_params_bebop2' );
 
 % control allocation
-fm_alt_hold.ca = controlAllocationWlsLoadParams( ...
-    'control_allocation_wls_params_quadcopter' );
+param.ca = loadParams( 'control_allocation_wls_params_quadcopter' );
 
 % sensor filter
-fm_alt_hold.sens_filt = indiSensFiltLoadParams( ...
-    'indi_sens_filt_params_default' );
+param.sens_filt = loadParams( 'indiSensFilt_params_default' );
 
 % motor time constant, in s
-fm_alt_hold.motor_time_constant = 0.0135;
+param.motor_time_constant = 0.0135;
 
 % flight mode sample time, in s
-fm_alt_hold.sample_time = 0.0025;
+param.sample_time = 0.0025;
