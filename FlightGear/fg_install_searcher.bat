7719179fa8d6e02beb26068153313d55bf44ffab
Yannic Beyer <yannic.beyer@gmx.de>
2023-01-04 21:53:19 +0100

    Automatically determine FlightGear version under Windows
    Under Windows the .bat scripts are used to start a FlightGear
    visualisation. In these scripts the FlightGear version was
    hard coded so that they did not work for different versions.
    The problem was solved by adding the .bat script
    fg_install_searcher.bat. This script searches for the
    FlightGear installation directory. If multiple FlightGear
    installations are found, the user is asked to select the
    desired version.

---
+++ FlightGear/fg_install_searcher.bat
@@ -0,0 +1,62 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set fg_versions_list=0 1 2 3 4
+set fg_parent_dir_list=0 1 2 3 4
+set fg_versions_nums=0 1 2 3 4
+set /A num_fg_versions=0
+set /A increment=1
+set folder_name=0
+
+set "fg_search_path=%programfiles%"
+cd %fg_search_path%
+call:fgSearcher
+
+set "fg_search_path=%programfiles(x86)%"
+cd %fg_search_path%
+call:fgSearcher
+
+if ("%num_fg_versions%"=="0") (
+	echo Error: FlightGear was NOT found!
+	pause
+)
+
+echo Found the following FlightGear version(s):
+
+for /l %%i in (1,1,%num_fg_versions%) do (
+	echo !fg_versions_nums[%%i]!  !fg_versions_list[%%i]!
+)
+
+set choice=1
+if %num_fg_versions% gtr 1 (
+	set /p choice="Please select one of the above options [1/2/...]:"
+)
+
+
+set folder_name=!fg_versions_list[%choice%]!
+set parent_folder_name=!fg_parent_dir_list[%choice%]!
+
+set fg_path=%parent_folder_name%/%folder_name%
+
+echo Starting FlightGear from: %fg_path%
+
+set fg_root=%fg_path%/data
+
+endlocal & (
+	set "FG_PATH=%fg_path%"
+	set "FG_ROOT=%fg_root%"
+)
+
+
+
+:fgSearcher
+for /d %%P in ("*") do (
+set folder_name=%%P
+if "!folder_name:~0,10!"=="FlightGear" (
+	set /a num_fg_versions=!num_fg_versions!+%increment%
+	set fg_versions_list[!num_fg_versions!]=%%P
+	set fg_versions_nums[!num_fg_versions!]=!num_fg_versions!
+	set "fg_parent_dir_list[!num_fg_versions!]=%fg_search_path%"
+	)
+)
+goto:eof
