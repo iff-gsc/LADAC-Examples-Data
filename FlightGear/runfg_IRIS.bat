eeeb02d4d0e88f868113dc393a8950b8070c4c02
Jonas Withelm <j.withelm@tu-braunschweig.de>
2023-05-24 17:49:15 +0200

    Change IRIS model propulsion animation
    - The propulsion animations in FlightGear have been changed back
    to the 'spin' animation type.
    - The 'To FlightGear' block in Simulink models does not need the
    integrator structure anymore (models need an update!).
    - Just multiply the 'motorBus.angular_velocity' with 'copter.config.propDir'
    to get the correct propulsion direction in FlightGear.
    - FlightGear must not be in pause mode, otherwise the 'spin' animation will not work!
    - Therefore, the '--enable-freeze' argument is removed from the IRIS FlightGear startup scripts.

--- FlightGear/runfg_IRIS.bat
+++ FlightGear/runfg_IRIS.bat
@@ -6,4 +6,4 @@ call fg_aircraft_path.bat
 
 cd %FG_PATH%
 
-.\\bin\fgfs --aircraft=IRIS --fdm=null --native-fdm=socket,in,30,localhost,5502,udp --native-ctrls=socket,out,30,127.0.0.1,5503,udp --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --enable-freeze --lat=37.6117 --lon=-122.3782 --altitude=7224 --heading=113 --offset-distance=4.72 --offset-azimuth=0
+.\\bin\fgfs --aircraft=IRIS --fdm=null --native-fdm=socket,in,30,localhost,5502,udp --native-ctrls=socket,out,30,127.0.0.1,5503,udp --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --lat=37.6117 --lon=-122.3782 --altitude=7224 --heading=113 --offset-distance=4.72 --offset-azimuth=0
