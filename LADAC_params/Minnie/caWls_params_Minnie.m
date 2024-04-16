a62d8b9d283ee6f76bc278e72105ffab9f7adbe1
Jonas Withelm <j.withelm@tu-braunschweig.de>
2024-04-16 10:31:23 +0200

    Add control allocation parameter file for Minnie

---
+++ LADAC_params/Minnie/caWls_params_Minnie.m
@@ -0,0 +1,19 @@
+% ** Parameters for wls control allocation (Minnie) **
+% 
+% See also:
+%   caWls_params_default.m
+
+% Disclaimer:
+%   SPDX-License-Identifier: GPL-3.0-only
+% 
+%   Copyright (C) 2020-2022 Yannic Beyer
+%   Copyright (C) 2024 TU Braunschweig, Institute of Flight Guidance
+% *************************************************************************
+
+% k is the number of control inputs
+% m is the number of pseudo control inputs
+
+param = loadParams( 'caWls_params_default' );
+
+% weighting mxm matrix of pseudo-control
+param.W_v = diag([30,30,0.01,300]);
